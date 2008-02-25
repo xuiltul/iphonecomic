c8823a632188c795e8b2379ac6d0362bff432812
yammtyostk <yammtyostk@80e3cb73-173f-0410-81d3-998474c5a783>
2008-02-25 16:24:11 +0000

    trunkの差分を反映

--- branches/yammtyostk/Global.h
+++ branches/yammtyostk/Global.h
@@ -3,11 +3,11 @@
 #define UITEXTLEN 1000
 
 #ifdef MOBILE
-	#define COMICVER @"iComic v009m"
+	#define COMICVER @"iComic v010m"
 	#define COMICPATH2 "/var/mobile/Media/Comic/"
 	#define COMICPATHLEN 24
 #else
-	#define COMICVER @"iComic v009r"
+	#define COMICVER @"iComic v010r"
 	#define COMICPATH2 "/var/root/Media/Comic/"
 	#define COMICPATHLEN 22
 #endif
